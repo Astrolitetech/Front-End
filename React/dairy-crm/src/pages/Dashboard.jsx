aa16bccc23db414ec1acd27b87a11e9745ebf1c0
Hrushikesh-Kanukuntla <hrushikesh.k@astrolitetech.com>
2025-06-05 15:22:05 +0530

    PushNewModules

--- React/dairy-crm/src/pages/Dashboard.jsx
+++ React/dairy-crm/src/pages/Dashboard.jsx
@@ -1,13 +1,22 @@
 // src/pages/Home.jsx
 import React from 'react';
 import Header from '../component/Header';
+import '../styles/dashboard.css'
 
 const Dashboard = () => {
   return (
     <>
       <Header title="Home Page" />
-      <main>
+      <main className='body'>
         <p>Welcome to the Home Page!</p>
+        <div>
+          <div>
+            <p>Welcome to my page</p>
+          </div>
+          <div>
+            <h2>hello</h2>
+          </div>
+        </div>
       </main>
     </>
   );
