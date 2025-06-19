a01004cb89208e862c5e94aa068a082137938ccf
Hrushikesh-Kanukuntla <hrushikesh.k@astrolitetech.com>
2025-06-19 10:46:13 +0530

    removed login

--- React/dairy-crm/src/App.js
+++ React/dairy-crm/src/App.js
@@ -3,25 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './component/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Milk from './pages/Milk';
-import LoginPage from './pages/Login';
 import Customers from './pages/Customers';
 
 function App() {
   return (
     <Router>
       <div className="flex">
-        <Sidebar />
-        <div className="flex-1 p-4">
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/milk" element={<Milk />} />
-          </Routes>
+        <div>
+          <Sidebar />
+          <div className="flex-1 p-4">
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/milk" element={<Milk />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
   );
-}  
+}
 
 export default App;
