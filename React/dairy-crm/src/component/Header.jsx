5c359b838295348017946560d500c82e8d8f2027
Hrushikesh-Kanukuntla <hrushikesh.k@astrolitetech.com>
2025-06-20 11:36:56 +0530

    updating header 0.0.2

--- React/dairy-crm/src/component/Header.jsx
+++ React/dairy-crm/src/component/Header.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import '../styles/header.css';
-import logo from '../assets/logo.jpg';
+import React, { useState } from 'react';
+import { Search, X, Bell, Settings, CircleUser } from 'lucide-react';
+import '../styles/header.css'; // Link your styles
+import logo from '../assets/logo.jpg'; // Update with your CRM logo path
 
 const Header = ({ title }) => {
+    const [query, setQuery] = useState('');
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+    const toggleSearch = () => {
+        if (isSearchOpen) setQuery('');
+        setIsSearchOpen(!isSearchOpen);
+    };
+
     return (
-        <header className='header'>
-            <img src={logo} className='logo-header' />
-            <h1 className='title'>{title}</h1>
+        <header className="crm-header">
+            <div className="header-left">
+                <img src={logo} alt="CRM Logo" className="crm-logo" />
+                <h1 className="crm-title">{title}</h1>
+            </div>
+
+            <div className="header-right">
+                <div className="header-center">
+                    <div className={`search-container ${isSearchOpen ? 'open' : ''}`}>
+                        {isSearchOpen && (
+                            <input
+                                type="text"
+                                placeholder="Search..."
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                                className="search-input"
+                            />
+                        )}
+                        {isSearchOpen ? (
+                            <X onClick={toggleSearch} className="search-icon" />
+                        ) : (
+                            <Search onClick={toggleSearch} className="search-icon" />
+                        )}
+                    </div>
+                </div>
+                <Bell className="icon" />
+                <Settings className="icon" />
+                <CircleUser className="icon" />
+            </div>
         </header>
     );
 };
